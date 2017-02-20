af339419886627ecf4feacca859af0ed1d4f1e13
Oleh <lzerocool@gmail.com>
2017-02-20 19:46:26 +0200

    init commit

---
+++ Basic_Lesson_6/src/Iv_6.java
@@ -0,0 +1,23 @@
+
+public class Iv_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			String [] names = {"ihor", "ira", "john", "vika" };
+			
+			for (int i = 0; i < names.length; i ++) {
+				
+			System.out.println("names[i]");
+			
+			}
+			
+			for(String n: names){
+				System.out.println(n);
+				
+			}
+				
+			}
+	}
+
+}
